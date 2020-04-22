37509cad6f745ffd2a88f9ecb44d4f9f22d12b01
Leah Blasczyk <BlasczykLeah@users.noreply.github.com>
2020-04-22 17:35:13 -0500

    started relearning everything in here

---
+++ EduRunTake2/Assets/Scripts/UnToggle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UnToggle : MonoBehaviour, IPointerClickHandler
+{
+    public Toggle[] otherToggles;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        foreach(Toggle t in otherToggles)
+        {
+            if (t.isOn) t.isOn = false;
+        }
+    }
+}
