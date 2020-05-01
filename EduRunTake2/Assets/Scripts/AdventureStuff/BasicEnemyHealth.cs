15795a0fb94062d1edf21b6d1382fa21fa06d229
WesMcW <wesmcwhorter@gmail.com>
2020-05-01 15:37:44 -0500

    moar enemy stuff

---
+++ EduRunTake2/Assets/Scripts/AdventureStuff/BasicEnemyHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyHealth : MonoBehaviour
+{
+    public int currHealth, maxHealth;
+    private float randX;
+    public GameObject Enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currHealth = maxHealth;
+        randX = Random.Range(-10f, 10f);
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        currHealth -= dmg;
+
+        if(currHealth <= 0)
+        {
+            Spawn();
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void Spawn()
+    {
+        Instantiate(Enemy, new Vector2(randX, 10), Quaternion.identity);
+    }
+}
---
+++ EduRunTake2/Assets/Scripts/AdventureStuff/BasicEnemyMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyMove : MonoBehaviour
+{
+    Transform target;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(gameObject.transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+}
