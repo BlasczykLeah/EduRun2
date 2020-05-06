e30ad2ffcca3e35f67678d7d4f372819c2ebfe41
WesMcW <wesmcwhorter@gmail.com>
2020-05-06 15:21:33 -0500

    Updated attack

--- EduRunTake2/Assets/Scripts/AdventureStuff/MeleeAttack.cs
+++ EduRunTake2/Assets/Scripts/AdventureStuff/MeleeAttack.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class MeleeAttack : MonoBehaviour
 {
+    [Header("Position of where to start the attack hitbox")]
     public Transform attackPos;
+    [Header("Radius for the attack hitbox")]
     public float attackRange = 0.5f;
+    [Header("Layers that are checked by the attack")]
     public LayerMask enemyLayers;
-
+    [Header("Damage per attack")]
     public int damage;
+    [Header("Cooldown between attacks")]
+    public float cooldown;
 
+    private float timeBtwnAtk;
     private Animator anim;
 
     // Start is called before the first frame update
@@ -29,14 +35,19 @@ public class MeleeAttack : MonoBehaviour
 
     void Attack()
     {
-        anim.SetTrigger("Attack");
-
-        Collider2D[] enemiesHit = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemyLayers);
-
-        foreach(Collider2D enemy in enemiesHit)
+        if (Time.time > timeBtwnAtk)
         {
-            Debug.Log("Enemy Hit!");
-            enemy.GetComponent<BasicEnemyHealth>().TakeDamage(damage);
+            timeBtwnAtk = Time.time + cooldown;
+
+            anim.SetTrigger("Attack");
+
+            Collider2D[] enemiesHit = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemyLayers);
+
+            foreach (Collider2D enemy in enemiesHit)
+            {
+                Debug.Log("Enemy Hit!");
+                enemy.GetComponent<BasicEnemyHealth>().TakeDamage(damage);
+            }
         }
     }
 
