e3f78b11e5af9c9d5800fe7e516a9985f3d83a4c
WesMcW <wesmcwhorter@gmail.com>
2020-05-15 23:25:47 -0500

    Finished sword anim

--- EduRunTake2/Assets/Scripts/AdventureStuff/StopGround.cs
+++ EduRunTake2/Assets/Scripts/AdventureStuff/StopGround.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class StopGround : MonoBehaviour
 {
     public Animator playerAnim;
+    public Animator swordAnim;
     public GameObject Boss;
 
     public void StopAnim()
     {
         playerAnim.SetBool("Stopped", true);
+        swordAnim.SetBool("Stop", true);
         Boss.SetActive(true);
     }
 }
