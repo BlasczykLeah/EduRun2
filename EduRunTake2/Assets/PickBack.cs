18fef74c573e80b9f8e1e4952d31b276d75f162c
WesMcW <wesmcwhorter@gmail.com>
2020-05-16 17:09:18 -0500

    Started background swap

---
+++ EduRunTake2/Assets/PickBack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickBack : MonoBehaviour
+{
+    public GameObject happy, sad;
+    private int randNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randNum = Random.Range(0, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
